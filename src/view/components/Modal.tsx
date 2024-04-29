c646b621f268060a2f530e287b5e742c96ebea6d
vi-lcandido <vitoria.lcandido@gmail.com>
2024-04-28 23:17:00 -0300

    cria novo modal para adicao de dados

--- src/view/components/Modal.tsx
+++ src/view/components/Modal.tsx
@@ -16,7 +16,7 @@ import "./modalStyles.css";
       <div className="fixed inset-0 flex items-center justify-center backdrop">
          <div className=" relative flex flex-col bg-slate-100 p-8 rounded-lg w-full max-w-xl">
             <div className="flex justify-between ">
-               <div className=" mb-4 border-b-2 border-b-amber-700 py-6  text-center">
+               <div className=" mb-4 py-6  text-center">
                   <div>
                   <h1> Edição dos dados</h1> 
                   </div>
