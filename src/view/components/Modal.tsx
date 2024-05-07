33af466c5b01b0e1536784a9f16af8a99f790219
vi-lcandido <vitoria.lcandido@gmail.com>
2024-05-06 22:26:32 -0300

    fix: modal adjustments

--- src/view/components/Modal.tsx
+++ src/view/components/Modal.tsx
@@ -1,40 +1,34 @@
-import { ReactNode } from "react";
-import "./modalStyles.css";
+import { ReactNode } from 'react';
+import './modalStyles.css';
 
- interface ModalProps {
-    isOpen: boolean;
-    handleClose: () => void;
-    children: ReactNode
- }
+interface ModalProps {
+  isOpen: boolean;
+  handleClose: () => void;
+  children: ReactNode;
+}
 
- export function Modal({isOpen, handleClose, children}: ModalProps){
-    if(!isOpen) return null;
+export function Modal({ isOpen, handleClose, children }: ModalProps) {
+  if (!isOpen) return null;
 
-
-    return (
-      <>
-      <div className="fixed inset-0 flex items-center justify-center backdrop">
-         <div className=" relative flex flex-col bg-slate-100 p-8 rounded-lg w-full max-w-xl">
-            <div className="flex justify-between ">
-               <div className=" mb-4 py-6  text-center">
-                  <div>
-                  <h1> Edição dos dados</h1> 
-                  </div>
-               </div>
-             <button 
-             className=""
-               type="button" 
-               onClick={handleClose}
-             >
-               X
-            </button>
+  return (
+    <>
+      <div className="fixed inset-0 z-50 flex items-center justify-center backdrop">
+        <div className=" relative bg-slate-100 p-8 rounded-lg w-full max-w-xl overflow-y-auto">
+          <div className="flex justify-between ">
+            <div className=" w-full mb-4 py-6 text-center border-b-amber-700">
+              <h1> Edição dos dados</h1>
             </div>
-            <div>{children}</div>
-         </div>
+            <button
+              className="absolute border-none bg-transparent m-4 text-gray-500 hover:text-red-600 transition-all font-semibold"
+              type="button"
+              onClick={handleClose}
+            >
+              X
+            </button>
+          </div>
+          <div>{children}</div>
+        </div>
       </div>
-
-
-      </>
-    ) 
-    
- }
+    </>
+  );
+}
